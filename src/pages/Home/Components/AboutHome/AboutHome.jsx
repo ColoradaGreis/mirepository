2695f5914efda03da8663f1e13db5cb0801f3b93
ColoradaGreis <graciana.baratti@gmail.com>
2023-03-13 21:18:50 -0300

    About me pero del home

--- src/pages/Home/Components/AboutHome/AboutHome.jsx
+++ src/pages/Home/Components/AboutHome/AboutHome.jsx
@@ -1,24 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import s from './AboutHome.module.css'
 
 function AboutHome () {
   return (
     <div className={s.about}>
 
-      <div>Home</div>
-      <div>Home</div>
+        <h1 className={s.h1}>About me</h1>
+        <p className={s.p}>
+          My name is Graciana Baratti.
+        I’m a Full Stack Developer based in Argentina.
+         I have a passion for learning and I’m always looking for new opportunities to grow and improve my skills...<Link to='/about' className={s.link}><p className={s.pl}>read more</p></Link>
+        </p>
 
-      <div>Home</div>
-
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
     </div>
 
   )
