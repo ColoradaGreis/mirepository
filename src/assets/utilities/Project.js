9cfa286fc154b1a4662aaa1f400117686cfea6ff
ColoradaGreis <graciana.baratti@gmail.com>
2023-03-20 20:56:49 -0300

    intento de card

---
+++ src/assets/utilities/Project.js
@@ -0,0 +1,12 @@
+export const Project = [{
+  img: '../assets/images/PF-wines.png',
+  name: 'E-Commerce Wine Store',
+  tools: ['React', 'Redux', 'Nodejs', 'Express', 'PostgreSQL', 'Sequelize', 'HTML5', 'CSS3', 'Bootstrap', 'Git', 'Github'],
+  url: 'https://e-wine-ashen.vercel.app'
+},
+{
+  img: '../assets/images/pi-videogames.png',
+  name: 'Video Games App',
+  tools: ['React', 'Redux', 'Nodejs', 'Express', 'PostgreSQL', 'Sequelize', 'HTML5', 'CSS3', 'Bootstrap', 'Git', 'Github'],
+  url: 'https://main--pi-videogames-greis.netlify.app'
+}]
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,36 +1,32 @@
 import React from 'react'
-import s from './Projects.module.css'
+// import s from './Projects.module.css'
+import { Project } from '../../assets/utilities/Project'
 
 function Projects () {
   return (
     <div>
-      <div className={s.cards}>
-        <div className={s.cards}>
-          <div className="card__image-holder">
-            <img className="card__image" src="https://source.unsplash.com/300x225/?wave" alt="wave" />
-          </div>
-          <div className="card-title">
-            <a href="#" className="toggle-info btn">
-              <span className="left"></span>
-              <span className="right"></span>
-            </a>
-            <h2>
-                Card title
-                <small>Image from unsplash.com</small>
-            </h2>
-          </div>
-          <div className="card-flap flap1">
-            <div className="card-description">
-              This grid is an attempt to make something nice that works on touch devices. Ignoring hover states when theyre not available etc.
-            </div>
-            <div className="card-flap flap2">
-              <div className="card-actions">
-                <a href="#" className="btn">Read more</a>
-              </div>
-            </div>
+
+    <div>
+      <h1>Projects</h1>
+    </div>
+    <div>
+
+    {
+      Project.map((project) => {
+        return (
+        <div className="card" key={project.name}>
+          <img src={project.img} className="card-img-top" alt={project.name}/>
+          <div className="card-body">
+            <h5 className="card-title">{project.name}</h5>
+              <p className="card-text">tools: {project.tools}</p>
+            <a href={project.url} className="btn btn-primary" target='_blank' rel='noreferrer'>see</a>
           </div>
         </div>
-      </div>
+
+        )
+      })
+    }
+    </div>
     </div>
   )
 }
