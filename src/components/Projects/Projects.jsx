925a2442a777c7d8c946da3d0f3f595b1d61945f
ColoradaGreis <graciana.baratti@gmail.com>
2023-03-20 21:04:58 -0300

    logré que se viera la foto

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-// import s from './Projects.module.css'
-import { Project } from '../../assets/utilities/Project'
+import s from './Projects.module.css'
+import wines from '../../assets/images/PF-wines.png'
+import pi from '../../assets/images/pi-videogames.png'
+// import { Project } from '../../assets/utilities/Project'
 
 function Projects () {
+  const Project = [{
+    img: wines,
+    name: 'E-Commerce Wine Store',
+    tools: ['React', 'Redux', 'Nodejs', 'Express', 'PostgreSQL', 'Sequelize', 'HTML5', 'CSS3', 'Bootstrap', 'Git', 'Github'],
+    url: 'https://e-wine-ashen.vercel.app'
+  },
+  {
+    img: pi,
+    name: 'Video Games App',
+    tools: ['React', 'Redux', 'Nodejs', 'Express', 'PostgreSQL', 'Sequelize', 'HTML5', 'CSS3', 'Bootstrap', 'Git', 'Github'],
+    url: 'https://main--pi-videogames-greis.netlify.app'
+  }]
   return (
     <div>
 
@@ -14,7 +28,7 @@ function Projects () {
     {
       Project.map((project) => {
         return (
-        <div className="card" key={project.name}>
+        <div className={`card ${s.card}`} key={project.name}>
           <img src={project.img} className="card-img-top" alt={project.name}/>
           <div className="card-body">
             <h5 className="card-title">{project.name}</h5>
